048fa41536dab1fb7070edf08432c59696fd917d
马馨怡 <maxinyi@maxinyideMacBook-Pro.local>
2025-11-30 12:12:28 +0900

    revise the base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kkoke01.github.io',
-  base:'/',
+  base:'astro-content',
   integrations: [
     vue({
       template: {
